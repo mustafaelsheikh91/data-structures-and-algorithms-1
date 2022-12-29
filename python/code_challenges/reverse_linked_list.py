78c5579aaf513c9cb1e4f8963d961b932973520a
unknown <ASU@1G06-24.asu.edu.jo>
2022-12-29 10:24:56 +0300

    done chalenge

--- python/code_challenges/reverse_linked_list.py
+++ python/code_challenges/reverse_linked_list.py
@@ -1,23 +1,59 @@
-   
-arr = [5, 2, 8, 7, 1];     
-temp = 0;    
-     
- 
-print("Elements of original array: ");    
-for i in range(0, len(arr)):    
-    print(arr[i], end=" ");    
-     
-for i in range(0, len(arr)):    
-    for j in range(i+1, len(arr)):    
-        if(arr[i] > arr[j]):    
-            temp = arr[i];    
-            arr[i] = arr[j];    
-            arr[j] = temp;    
-     
-print();    
-     
+# Python code to demonstrate Implementing
+# stack using list
+stack = ["Amar", "Akbar", "Anthony"]
+stack.append("Ram")
+stack.append("Iqbal")
+print(stack)
 
-    
-print("Elements of array sorted in ascending order: ");    
-for i in range(0, len(arr)):    
-    print(arr[i], end=" ");    
+# Removes the last item
+print(stack.pop())
+
+print(stack)
+
+# Removes the last item
+print(stack.pop())
+
+print(stack)
+
+# Python code to demonstrate Implementing
+# Queue using list
+queue = ["Amar", "Akbar", "Anthony"]
+queue.append("Ram")
+queue.append("Iqbal")
+print(queue)
+
+# Removes the first item
+print(queue.pop(0))
+
+print(queue)
+
+# Removes the first item
+print(queue.pop(0))
+
+print(queue)
+
+# Python code to demonstrate Implementing
+# Stack using deque
+from collections import deque
+queue = deque(["Ram", "Tarun", "Asif", "John"])
+print(queue)
+queue.append("Akbar")
+print(queue)
+queue.append("Birbal")
+print(queue)
+print(queue.pop())				
+print(queue.pop())				
+print(queue)
+
+# Python code to demonstrate Implementing 
+# Queue using deque
+from collections import deque
+queue = deque(["Ram", "Tarun", "Asif", "John"])
+print(queue)
+queue.append("Akbar")
+print(queue)
+queue.append("Birbal")
+print(queue)
+print(queue.popleft())                 
+print(queue.popleft())                 
+print(queue)

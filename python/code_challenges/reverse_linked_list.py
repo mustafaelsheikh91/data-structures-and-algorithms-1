6ca593b4079d8a45f0ffb9234e8cd1156a02cce0
unknown <ASU@1G06-24.asu.edu.jo>
2022-12-29 10:10:55 +0300

    do the challeng

--- python/code_challenges/reverse_linked_list.py
+++ python/code_challenges/reverse_linked_list.py
@@ -1,9 +1,23 @@
-def reverse_list(ll):
-    """Reverses a linked list
-    Args:
-        ll: linked list
-    Returns:
-        linked list in reversed form
-    """
-    # put your function implementation here
-    return ll
+   
+arr = [5, 2, 8, 7, 1];     
+temp = 0;    
+     
+ 
+print("Elements of original array: ");    
+for i in range(0, len(arr)):    
+    print(arr[i], end=" ");    
+     
+for i in range(0, len(arr)):    
+    for j in range(i+1, len(arr)):    
+        if(arr[i] > arr[j]):    
+            temp = arr[i];    
+            arr[i] = arr[j];    
+            arr[j] = temp;    
+     
+print();    
+     
+
+    
+print("Elements of array sorted in ascending order: ");    
+for i in range(0, len(arr)):    
+    print(arr[i], end=" ");    
